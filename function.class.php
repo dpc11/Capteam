f6b9266ae42b72a1a9cfde014d0ed55ff254b8b6
12301121-金林荣 <564257051@qq.com>
2015-04-22 11:44:02 +0800

    更新了select_user函数

--- function.class.php
+++ function.class.php
@@ -1180,6 +1180,30 @@ $tktype_arr[$row_tktype['id']]['task_tpye'] =  $row_tktype['task_tpye'];
 return $tktype_arr;
 }
 
+//获得所有的用户信息
+function get_all_user_select() {
+global $tankdb;
+global $database_tankdb;
+  
+$query_user ="SELECT * FROM tk_user ORDER BY CONVERT(tk_display_name USING gbk )";
+// $query_user ="SELECT * 
+// FROM tk_user 
+// inner join tk_team on tk_team.tk_team_uid=tk_user.uid 
+// WHERE tk_team.tk_team_pid = $prjid ORDER BY CONVERT(tk_display_name USING gbk )";
+$userRS = mysql_query($query_user, $tankdb) or die(mysql_error());
+$row_user = mysql_fetch_assoc($userRS);
+ 
+$user_arr = array ();
+do { 
+
+$user_arr[$row_user['uid']]['uid'] =  $row_user['uid'];
+$user_arr[$row_user['uid']]['name'] =  $row_user['tk_display_name'];
+$user_arr[$row_user['uid']]['email'] =  $row_user['tk_user_email'];
+$user_arr[$row_user['uid']]['phone_num'] =  $row_user['tk_user_contact'];
+} while ($row_user = mysql_fetch_assoc($userRS));     
+    
+return $user_arr;
+}
 
 //get user
 function get_user_select($prjid) {
