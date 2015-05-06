d8a545ebfc38a9d52c14e605a5e8eb4160787923
jiaojinnaozhixiangmingzi <wanghaixin>
2015-05-06 11:57:35 +0800

    编辑项目删除和增加成员log显示完成

--- project_edit.php
+++ project_edit.php
@@ -105,12 +105,14 @@ $Recordset2 = mysql_query($query_deleterecordset, $tankdb) or die(mysql_error())
 
 $totalRows_Recordset2 = mysql_num_rows($Recordset2);
 
-  $user_arr = $user_dao_obj->get_user_select_by_project($colname_DetailRS1);
+  
   while($row = mysql_fetch_array($Recordset2,MYSQL_ASSOC))
 {
       $arr[]=$row;
 }
-print_r($arr);
+
+
+
 //删除原有的项目成员
 $deleteMemSQL = sprintf("DELETE from tk_team WHERE tk_team_pid = %s", GetSQLValueString($colname_Recordset1, "int"));
 mysql_select_db($database_tankdb, $tankdb);
@@ -152,6 +154,52 @@ $user_list= $_POST['project_to_user'];
         mysql_select_db($database_tankdb, $tankdb);
         $Result1 = mysql_query($addnewmemSQL, $tankdb) or die(mysql_error());
     }
+
+    $arr1[] = array(); //将要被添加的成员数组
+mysql_select_db($database_tankdb, $tankdb);
+$Recordset3 = mysql_query($query_deleterecordset1, $tankdb) or die(mysql_error());
+$totalRows_Recordset3 = mysql_num_rows($Recordset3);
+
+  while($row1 = mysql_fetch_array($Recordset3,MYSQL_ASSOC))
+{  
+      $arr1[]=$row1;
+}
+
+$ids1 = array();
+$ids1 = array_reduce($arr, create_function('$v,$w', '$v[$w["tk_team_uid"]]=$w["tk_team_uid"];return $v;'));
+print_r($ids1);
+$ids2 = array();
+$ids2 = array_reduce($arr1, create_function('$v,$w', '$v[$w["tk_team_uid"]]=$w["tk_team_uid"];return $v;'));
+print_r($ids2);
+
+$onemoretwo=array();
+$onemoretwo=array_diff($ids2,$ids1);
+print_r($onemoretwo);
+
+mysql_select_db($database_tankdb, $tankdb);
+foreach($onemoretwo as $a){//在循环中只要用到值
+date_default_timezone_set('PRC');
+    $action='删除了成员:'.$a;
+              $timenow=date('Y-m-d H:i:s',time());
+              $insertSQLLog=sprintf("INSERT into tk_log(tk_log_user,tk_log_action,tk_log_time,tk_log_type,tk_log_class)
+ 
+               
+              $Result2 = mysql_query($insertSQLLog, $tankdb) or die(mysql_error());}
+
+
+$twomoreone=array();
+$twomoreone=array_diff($ids1,$ids2);
+print_r($twomoreone);
+
+mysql_select_db($database_tankdb, $tankdb);
+foreach($twomoreone as $b){//在循环中只要用到值
+date_default_timezone_set('PRC');
+    $action='添加了成员:'.$b;
+              $timenow=date('Y-m-d H:i:s',time());
+              $insertSQLLog=sprintf("INSERT into tk_log(tk_log_user,tk_log_action,tk_log_time,tk_log_type,tk_log_class)
+ 
+               
+              $Result2 = mysql_query($insertSQLLog, $tankdb) or die(mysql_error());}
 }
 
 ?>
