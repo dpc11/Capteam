d170f12f0801e39b972cd2334ef6c578f9eef39b
12301137-yangxin <12301137@bjtu.edu.cn>
2015-05-17 00:45:04 +0800

    修改js文件，文字过长用省略号显示，并保留原本链接

--- js/jquery/jquery.ellipsis.js
+++ js/jquery/jquery.ellipsis.js
@@ -9,11 +9,24 @@
             max = $elem.width();
         }
         max = max * options.maxLine;
-        var text = $.trim($elem.text()).replace(' ','　'); //for fix white-space bug　
+		var text=$elem[0].innerHTML;
+		var left="";
+		var right="";
+		if(text.indexOf("<a ")>=0){
+			var arr = text.split('>');
+			left=arr[0];right=arr[1];
+			arr=right.split('<');
+			right=arr[1];
+			text=$elem.text();
+		}
+		var text = $.trim($elem.text()).replace(' ','　');
         var $temp_elem = $elem.clone(false)
             .css({ "visibility": "hidden", "whiteSpace": "nowrap","width":"auto" })
             .appendTo(document.body);
         var width = $temp_elem.width();
+		if($temp_elem.width()==0){
+			width = $temp_elem[0].innerHTML.length;
+		}
 		if(width > max){
 			var stop =  Math.floor(text.length * max / width); // for performance while content exceeding the limit substantially
 			var temp_str = text.substring(0,stop) + ellipsis_char;
@@ -23,6 +36,10 @@
 					stop--;
 					temp_str = text.substring(0, stop) + ellipsis_char;
 					width = $temp_elem.text(temp_str).width();
+					if(width==0){
+						$temp_elem[0].innerHTML=temp_str;
+						width = $temp_elem[0].innerHTML.length;
+					}
 				}					
 			}
 			else if(width < max){
@@ -30,12 +47,22 @@
 					stop++;
 					temp_str = text.substring(0, stop) + ellipsis_char;
 					width = $temp_elem.text(temp_str).width();
+					if(width==0){
+						$temp_elem[0].innerHTML=temp_str;
+						width = $temp_elem[0].innerHTML.length;
+					}
 				}
 				if(width > max){
 					temp_str = text.substring(0,stop -1)+ellipsis_char;
 				}
 			}
-			$elem.text(temp_str.replace('　',' '));
+			if(left!=""){
+				$elem[0].innerHTML=left+'>'+temp_str.replace('　',' ')+'<'+right+'>';
+			}else if($elem.width()!=0){
+				$elem.text(temp_str.replace('　',' '));
+			}else{
+				$elem[0].innerHTML=temp_str.replace('　',' ');
+			}
 		}
         $temp_elem.remove();
     }
