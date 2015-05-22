f4724081b207ec9c190cc4452bc6cf73cc556429
jiaojinnaozhixiangmingzi <1152352921@qq.com>
2015-05-22 22:20:35 +0800

    页面显示，入口http://localhost/Capteam/user_find_pass.php

--- forget_pass.php
+++ forget_pass.php
@@ -7,9 +7,6 @@
 <html>
 <head>
 <meta charset="utf-8">
-<meta name="keywords" content="找回密码,php" />
-<meta name="description" content="Helloweba演示平台，演示HTML、CSS、jquery、PHP实例" />
-
 <link rel="stylesheet" type="text/css" href="../css/main.css" />
 <style type="text/css">
 .demo{width:400px; margin:40px auto 0 auto; min-height:250px;}
@@ -20,35 +17,12 @@
 .btn{position: relative;overflow: hidden;display:inline-block;*display:inline;padding:4px 20px 4px;font-size:16px;line-height:20px;*line-height:22px;color:#fff;text-align:center;vertical-align:middle;cursor:pointer;background-color:#5bb75b;border:1px solid #cccccc;border-color:#e6e6e6 #e6e6e6 #bfbfbf;border-bottom-color:#b3b3b3;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;}
 </style>
 <script type="text/javascript" src="../js/jquery.js"></script>
-<!--<script type="text/javascript">
-$(function(){
-	$("#sub_btn").click(function(){
-		var email = $("#email").val();
-		var preg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/; //匹配Email
-		if(email=='' || !preg.test(email)){
-			$("#chkmsg").html("请填写正确的邮箱！");
-		}else{
-			$("#sub_btn").attr("disabled","disabled").val('提交中..').css("cursor","default");
-			$.post("sendmail.php",{mail:email},function(msg){
-				if(msg=="noreg"){
-					$("#chkmsg").html("该邮箱尚未注册！");
-					$("#sub_btn").removeAttr("disabled").val('提 交').css("cursor","pointer");
-				}else{
-					$(".demo").html("<h3>"+msg+"</h3>");
-				}
-			});
-		}
-	});
-})
-</script>-->
-
-
 
 </head>
 
 <body>
 
-
+<?php require_once('head.php'); ?>
 <div id="main">
   
    <div class="demo">
